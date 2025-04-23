926a671b9afe2c4c78b9fe8f3e579354b96dc85e
giapts <nguyentronggiap317@gmail.com>
2025-04-24 00:09:57 +0700

    ho khau

--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,34 +1,42 @@
 package com.example.demo.exception;
 
 import com.example.demo.dto.ApiResponse;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.Map;
 
 //????
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiResponse<Object>> handleResourceNotFound(NotFoundException ex) {
+    public ResponseEntity<ApiResponse<Object>> handleResourceNotFound(NotFoundException ex,  HttpServletRequest request) {
         ApiResponse<Object> apiResponse = new ApiResponse<>(
                 "not_found",
                 ex.getMessage(),
                 null,
-                null
+                Map.of(
+                        "timestamp", Instant.now(),
+                        "path", request.getRequestURI()
+                )
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Object>> handleOtherExceptions(Exception ex) {
+    public ResponseEntity<ApiResponse<Object>> handleOtherExceptions(Exception ex,  HttpServletRequest request) {
         ApiResponse<Object> apiResponse = new ApiResponse<>(
                 "error",
                 ex.getMessage(),
                 null,
-                null
+                Map.of(
+                        "timestamp", Instant.now(),
+                        "path", request.getRequestURI()
+                )
         );
         return ResponseEntity.status(500).body(apiResponse);
     }
--- demo/src/main/java/com/example/demo/service/Impl/HouseholdServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/Impl/HouseholdServiceImpl.java
@@ -1,8 +1,87 @@
 package com.example.demo.service.Impl;
 
+
+import com.example.demo.dto.HouseholdDto;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.models.Household;
+import com.example.demo.repositories.HouseholdRepository;
 import com.example.demo.service.HouseholdService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class HouseholdServiceImpl implements HouseholdService {
+    @Autowired
+    private HouseholdRepository householdRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public List<HouseholdDto> getAllHouseholds() {
+        List<Household> households = householdRepository.findAll();
+        // Convert List<Household> to List<HouseholdDto>
+        List<HouseholdDto> householdDtos = households.stream()
+                .map(household -> modelMapper.map(household, HouseholdDto.class))
+                .toList();
+       return householdDtos;
+    }
+
+    @Override
+    public HouseholdDto addHousehold(HouseholdDto householdDto) {
+        // Convert DTO to entity
+        Household household = modelMapper.map(householdDto, Household.class);
+        // Save entity to the database
+        Household savedHousehold = householdRepository.save(household);
+
+        return modelMapper.map(savedHousehold, HouseholdDto.class);
+    }
+
+    @Override
+    public HouseholdDto getHouseholdById(Long id) {
+        // Find the household by ID
+        Household household = householdRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Household not found")
+        );
+
+        return modelMapper.map(household, HouseholdDto.class);
+    }
+
+    @Override
+    public HouseholdDto updateHousehold(Long id, HouseholdDto householdDto) {
+        // Find the existing household by ID
+        Household existingHousehold = householdRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Household not found")
+        );
+        // Update the existing household with new values
+        existingHousehold.setOwnerName(householdDto.getOwnerName());
+        existingHousehold.setNumberOfHousehold(householdDto.getNumberOfHousehold());
+        existingHousehold.setLocation(householdDto.getLocation());
+        existingHousehold.setAreaCode(householdDto.getAreaCode());
+        existingHousehold.setNumberOfPeople(householdDto.getNumberOfPeople());
+        existingHousehold.setCitizenIdOwner(householdDto.getCitizenIdOwner());
+        existingHousehold.setUpdateAt(new Date());
+        // Save the updated household to the database
+        Household updatedHousehold = householdRepository.save(existingHousehold);
+
+
+        return modelMapper.map(updatedHousehold, HouseholdDto.class);
+    }
+
+    @Override
+    public String deleteHousehold(Long id) {
+        // Find the household by ID
+        Household existingHousehold = householdRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Household not found")
+        );
+        // Delete the household from the database
+        householdRepository.delete(existingHousehold);
+
+        return "Household with ID " + id + " deleted successfully";
+    }
+
+
 }
