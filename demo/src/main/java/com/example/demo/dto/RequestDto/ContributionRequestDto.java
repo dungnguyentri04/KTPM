3423021f6b995f34e79d919f48e3912d5069f1a6
Vietpn1909 <vietpn04@gmail.com>
2025-06-07 22:05:35 +0700

    ContributionDto

--- demo/src/main/java/com/example/demo/dto/RequestDto/ContributionRequestDto.java
+++ demo/src/main/java/com/example/demo/dto/RequestDto/ContributionRequestDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.Map;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +14,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContributionRequestDto {
+    @NotNull(message = "Fee id is required")
     private Long feeId;
 
+    @NotNull(message = "Household id is required")
     private Long householdId;
 
+    @NotNull(message = "Money is required")
     private double money;
 
 //    private double price;
 
+    @NotNull(message = "Payment date is required")
     private LocalDate deadline;
 
     private int vehicle;
