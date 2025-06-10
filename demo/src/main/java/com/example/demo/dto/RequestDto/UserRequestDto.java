8d67f4a541956bacd90bb1add7f45e9081df9193
vannghj <phanvannghi04@gmail.com>
2025-06-10 19:50:46 +0700

    update nghi

--- demo/src/main/java/com/example/demo/dto/RequestDto/UserRequestDto.java
+++ demo/src/main/java/com/example/demo/dto/RequestDto/UserRequestDto.java
@@ -21,4 +21,21 @@ public class UserRequestDto {
 
     @NotBlank(message = "User phone number is required")
     private String phoneNumber;
+
+    private String avatar;
+    private String address;
+    private String email;
+    private String fullName;
+    private String citizenId;
+
+    @NotNull(message = "User status is required")
+    private Boolean status;
+
+    private String created_at;
+    private String updated_at;
+
+    private String created_by;
+    private String updated_by;
+
+
 }
